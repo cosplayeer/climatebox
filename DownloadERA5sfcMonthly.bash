e7d5d831b20a7ed4f3269dc35571c28b9dbfe9e9
DYP <duanyapeng1@163.com>
2019-07-22 18:05:53 +0800

    12 month

--- DownloadERA5sfcMonthly.bash
+++ DownloadERA5sfcMonthly.bash
@@ -5,8 +5,8 @@ input_end=$2
 
 function run_demo(){
     year=`echo ${timetemp} | cut -c 1-4`
-    #monthlist="01 02 03 04 05 06 07 08 09 10 11 12"
-    monthlist="01 02"
+    monthlist="01 02 03 04 05 06 07 08 09 10 11 12"
+    #monthlist="01 02"
     for imonth in $monthlist; do
     echo "Download ${timetemp} ${imonth}..."
     python DownloadERA5sfcMonthlytemp.py --year $year --month $imonth
