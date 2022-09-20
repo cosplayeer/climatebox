1982a46093a9986f3918fc5cc3a7087316e25c49
cosplayeer <duanyapeng1@163.com>
2022-09-20 09:44:30 +0800

    a

--- CopernicusDownloadScript/history/Atmosphere/ERA5SolarDaily/scripts/readRadMonthly.py
+++ CopernicusDownloadScript/history/Atmosphere/ERA5SolarDaily/scripts/readRadMonthly.py
@@ -67,8 +67,18 @@ class Radiation:
         df_era5 = self.readinERA5(fname=fnameERA5)
         df_cams = self.readinCFSv2(fname=fnameCFS)
         df = pd.concat([df_era5, df_cams], axis=1)
-        df['GHI_era5_cams'] = (df['GHI_ERA5_ssrc'] + df['GHI_CFSv2']) / 2
+        df['GHI_era5_cfs'] = (df['GHI_ERA5_ssrc'] + df['GHI_CFSv2']) / 2
         df2 = df.drop(labels='GHI_ERA5_ssrc', axis=1)
+        df2 = df2.drop(labels='GHI_CFSv2', axis=1)
+        # print(df2)
+        return df2
+
+    def readinCAMSAndCFSv2(self, fnameCAMS, fnameCFS):
+        df_cams = self.readinCams(fname=fnameCAMS)
+        df_cfs = self.readinCFSv2(fname=fnameCFS)
+        df = pd.concat([df_cams, df_cfs], axis=1)
+        df['GHI_cams_cfs'] = (df['GHI_cams'] + df['GHI_CFSv2']) / 2
+        df2 = df.drop(labels='GHI_cams', axis=1)
         df2 = df2.drop(labels='GHI_CFSv2', axis=1)
         # print(df2)
         return df2
@@ -96,13 +106,17 @@ class Radiation:
         s = df.loc[(df.index < datetime(self.endyear, self.endmonth, self.endday)) &
                    (df.index >= datetime(self.startyear, self.startmonth, self.startday))]
         return s
+    # sheet 2
 
     def RMSEwithObs(self, fname, obsname, fname2=''):
         s = {}
         dfObs = self.readinObs(obsname)
         if self.dtype == 'Era5Cams':
             dfRad = self.readinERA5AndCAMS(fname, fname2)
-            # print(dfRad)
+        if self.dtype == 'Era5Cfsv2':
+            dfRad = self.readinERA5AndCFSv2(fname, fname2)
+        if self.dtype == 'CamsCFSv2':
+            dfRad = self.readinCAMSAndCFSv2(fname, fname2)
         elif self.dtype == 'meteonorm':
             dfRad = self.readinMeteoNorm(fname)
         elif self.dtype == 'era5':
@@ -124,12 +138,20 @@ class Radiation:
         # s to csv
         s['13'] = '{:.2f}'.format(calculateRMSE(df))
         s2 = pd.DataFrame.from_dict(s, orient='index')
-        if fname2 != '':
-            s2.to_csv('text/RMSE_ERA5_CAMS'+fname)
+        if 'ecmwf_radiation' in fname:
+            if 'CAMS' in fname2:
+                s2.to_csv('text/RMSE_ERA5_CAMS'+fname)
+            elif 'cfsv2' in fname2:
+                s2.to_csv('text/RMSE_ERA5_CFSV2'+fname)
+            else:
+                s2.to_csv('text/RMSE'+fname)
+        if 'CAMS' in fname and 'cfsv2' in fname2:
+            s2.to_csv('text/RMSE_CAMS_CFSv2'+fname)
         else:
             s2.to_csv('text/RMSE'+fname)
+# sheet 1
 
-    def processSumMonth(self, fname):
+    def processSumMonth(self, fname, fname2=''):
         s = dict()
         if self.dtype == 'obs':
             df = self.readinObs(fname)
@@ -141,6 +163,12 @@ class Radiation:
             df = self.readinCams(fname)
         elif self.dtype == 'cfsv2':
             df = self.readinCFSv2(fname)
+        elif self.dtype == 'Era5Cfsv2':
+            df = self.readinERA5AndCFSv2(fname, fname2)
+        elif self.dtype == 'Era5Cams':
+            df = self.readinERA5AndCAMS(fname, fname2)
+        elif self.dtype == 'CamsCFSv2':
+            df = self.readinCAMSAndCFSv2(fname, fname2)
 
         for imonth in range(1, 13):
             startdate = datetime(self.startyear, imonth, self.startday)
@@ -165,13 +193,34 @@ class Radiation:
             elif self.dtype == 'cfsv2':
                 s[str(imonth)] = '{:.2f}'.format(
                     _idata['GHI_CFSv2'].sum() / 1000)
+            elif self.dtype == 'Era5Cfsv2':
+                s[str(imonth)] = '{:.2f}'.format(
+                    _idata['GHI_era5_cfs'].sum() / 1000)
+            elif self.dtype == 'Era5Cams':
+                s[str(imonth)] = '{:.2f}'.format(
+                    _idata['GHI_era5_cams'].sum() / 1000)
+            elif self.dtype == 'CamsCFSv2':
+                s[str(imonth)] = '{:.2f}'.format(
+                    _idata['GHI_cams_cfs'].sum() / 1000)
         # s to csv
-        s['13'] = '{:.2f}'.format(self.processSumYear(fname))
+        s['13'] = '{:.2f}'.format(self.processSumYear(fname, fname2))
         s2 = pd.DataFrame.from_dict(s, orient='index')
-        s2.to_csv('text/Monthly'+fname)
+        if 'ecmwf_radiation' in fname:
+            if 'CAMS' in fname2:
+                s2.to_csv('text/Monthly_ERA5_CAMS'+fname)
+            elif 'cfsv2' in fname2:
+                s2.to_csv('text/Monthly_ERA5_CFSV2'+fname)
+            else:
+                s2.to_csv('text/Monthly'+fname)
+        if 'CAMS' in fname and 'cfsv2' in fname2:
+            s2.to_csv('text/Monthly_CAMS_CFSv2'+fname)
+
+        else:
+            s2.to_csv('text/Monthly'+fname)
+
         # return s
 
-    def processSumYear(self, fname):
+    def processSumYear(self, fname, fname2=''):
         if self.dtype == 'obs':
             df = self.readinObs(fname)
             result = df['GHI_obs'].sum() / 1000
@@ -187,13 +236,18 @@ class Radiation:
         elif self.dtype == 'cfsv2':
             df = self.readinCFSv2(fname)
             result = df['GHI_CFSv2'].sum() / 1000
-
+        elif self.dtype == 'Era5Cfsv2':
+            df = self.readinERA5AndCFSv2(fname, fname2)
+            result = df['GHI_era5_cfs'].sum() / 1000
+        elif self.dtype == 'Era5Cams':
+            df = self.readinERA5AndCAMS(fname, fname2)
+            result = df['GHI_era5_cams'].sum() / 1000
+        elif self.dtype == 'CamsCFSv2':
+            df = self.readinCAMSAndCFSv2(fname, fname2)
+            result = df['GHI_cams_cfs'].sum() / 1000
         # print(result)
         return result
 
-    def prt(self, fname):
-        result = self.readinObs(fname)
-        print(result)
 
 # month mean bias
 # obs = Radiation(startyear=2018, startmonth=1, startday=1,
@@ -230,8 +284,29 @@ class Radiation:
 # cfs.processSumMonth(fname='cfsv2_radiation_fuxin_demo.csv')
 # cfs.processSumMonth(fname='cfsv2_radiation_linuo_demo.csv')
 
+#  demo 2 input
+test1 = Radiation(startyear=2018, startmonth=1, startday=1,
+                  #                  # Era5Cfsv2
+                  #                  #  endyear=2019, endmonth=1, endday=1, dtype='Era5Cfsv2')
+                  #                  # Era5Cams
+                  #                  endyear=2019, endmonth=1, endday=1, dtype='Era5Cams')
+                  # CamsCFSv2
+                  endyear=2019, endmonth=1, endday=1, dtype='CamsCFSv2')
+test1.processSumMonth(
+    # fname='ecmwf_radiation_dongfang_demo.csv',
+    fname='CAMSdongfangminshengYear2018.csv',
+    fname2='cfsv2_radiation_dongfang_demo.csv')
+test1.processSumMonth(
+    # fname='ecmwf_radiation_fuxin_demo.csv',
+    fname='CAMSfuxinmuleiYear2018.csv',
+    fname2='cfsv2_radiation_fuxin_demo.csv')
+test1.processSumMonth(
+    # fname='ecmwf_radiation_linuo_demo.csv',
+    fname='CAMSlinuoYear2018.csv',
+    fname2='cfsv2_radiation_linuo_demo.csv')
 
-# month RMSE
+
+# sheet 2. month RMSE
 # cams = Radiation(startyear=2018, startmonth=1, startday=1,
 #                  endyear=2019, endmonth=1, endday=1, dtype='cams')
 # cams.RMSEwithObs(fname='CAMSdongfangminshengYear2018.csv',
@@ -274,12 +349,50 @@ class Radiation:
 #                  fname2='CAMSlinuoYear2018.csv',
 #                  obsname='新疆力诺石城子_1h-UTC0Exported.txt')
 
+# month RMSE：　cfs test
+# cfs = Radiation(startyear=2018, startmonth=1, startday=1,
+#                 endyear=2019, endmonth=1, endday=1, dtype='cfsv2')
+# cfs.RMSEwithObs(fname='cfsv2_radiation_dongfang_demo.csv',
+#                 obsname='新疆东方民生石城子_1h-UTC0Exported.txt')
+# cfs.RMSEwithObs(fname='cfsv2_radiation_fuxin_demo.csv',
+#                 obsname='福新木垒光伏一电站_1h-UTC0Exported.txt')
+# cfs.RMSEwithObs(fname='cfsv2_radiation_linuo_demo.csv',
+#                 obsname='新疆力诺石城子_1h-UTC0Exported.txt')
+
+# month RMSE：　ERA5  && CFSV2 test
+# test = Radiation(startyear=2018, startmonth=1, startday=1,
+#                  endyear=2019, endmonth=1, endday=1, dtype='Era5Cfsv2')
+# test.RMSEwithObs(fname='ecmwf_radiation_dongfang_demo.csv',
+#                  fname2='cfsv2_radiation_dongfang_demo.csv',
+#                  obsname='新疆东方民生石城子_1h-UTC0Exported.txt')
+# test.RMSEwithObs(fname='ecmwf_radiation_fuxin_demo.csv',
+#                  fname2='cfsv2_radiation_fuxin_demo.csv',
+#                  obsname='福新木垒光伏一电站_1h-UTC0Exported.txt')
+# test.RMSEwithObs(fname='ecmwf_radiation_linuo_demo.csv',
+#                  fname2='cfsv2_radiation_linuo_demo.csv',
+#                  obsname='新疆力诺石城子_1h-UTC0Exported.txt')
+
 # month RMSE：　ERA5  && CAMS test
-cfs = Radiation(startyear=2018, startmonth=1, startday=1,
-                endyear=2019, endmonth=1, endday=1, dtype='cfsv2')
-cfs.RMSEwithObs(fname='cfsv2_radiation_dongfang_demo.csv',
-                obsname='新疆东方民生石城子_1h-UTC0Exported.txt')
-cfs.RMSEwithObs(fname='cfsv2_radiation_fuxin_demo.csv',
-                obsname='福新木垒光伏一电站_1h-UTC0Exported.txt')
-cfs.RMSEwithObs(fname='cfsv2_radiation_linuo_demo.csv',
-                obsname='新疆力诺石城子_1h-UTC0Exported.txt')
+# test2 = Radiation(startyear=2018, startmonth=1, startday=1,
+#                  endyear=2019, endmonth=1, endday=1, dtype='Era5Cams')
+# test2.RMSEwithObs(fname='ecmwf_radiation_dongfang_demo.csv',
+#                  fname2='CAMSdongfangminshengYear2018.csv',
+#                  obsname='新疆东方民生石城子_1h-UTC0Exported.txt')
+# test2.RMSEwithObs(fname='ecmwf_radiation_fuxin_demo.csv',
+#                  fname2='CAMSfuxinmuleiYear2018.csv',
+#                  obsname='福新木垒光伏一电站_1h-UTC0Exported.txt')
+# test2.RMSEwithObs(fname='ecmwf_radiation_linuo_demo.csv',
+#                  fname2='CAMSlinuoYear2018.csv',
+#                  obsname='新疆力诺石城子_1h-UTC0Exported.txt')
+# month CAMS  && CFSv2 test
+test2 = Radiation(startyear=2018, startmonth=1, startday=1,
+                  endyear=2019, endmonth=1, endday=1, dtype='CamsCFSv2')
+test2.RMSEwithObs(fname='CAMSdongfangminshengYear2018.csv',
+                  fname2='cfsv2_radiation_dongfang_demo.csv',
+                  obsname='新疆东方民生石城子_1h-UTC0Exported.txt')
+test2.RMSEwithObs(fname='CAMSfuxinmuleiYear2018.csv',
+                  fname2='cfsv2_radiation_fuxin_demo.csv',
+                  obsname='福新木垒光伏一电站_1h-UTC0Exported.txt')
+test2.RMSEwithObs(fname='CAMSlinuoYear2018.csv',
+                  fname2='cfsv2_radiation_linuo_demo.csv',
+                  obsname='新疆力诺石城子_1h-UTC0Exported.txt')
